3cffa89d6d8f97625ce56ad1f467898ed7e05edc
DanZo0x <dan.cheype@gmail.com>
2023-04-14 19:44:07 +0200

    All base TUs done

--- Assets/_FightSystem/Level 2/Character.cs
+++ Assets/_FightSystem/Level 2/Character.cs
@@ -28,6 +28,9 @@ namespace _2023_GC_A2_Partiel_POO.Level_2
         /// </summary>
         TYPE _baseType;
 
+        Equipment _currentEquipment;
+        int _maxHealth;
+
         public Character(int baseHealth, int baseAttack, int baseDefense, int baseSpeed, TYPE baseType)
         {
             _baseHealth = baseHealth;
@@ -35,51 +38,52 @@ namespace _2023_GC_A2_Partiel_POO.Level_2
             _baseDefense = baseDefense;
             _baseSpeed = baseSpeed;
             _baseType = baseType;
+            _maxHealth = _baseHealth;
         }
         /// <summary>
         /// HP actuel du personnage
         /// </summary>
-        public int CurrentHealth { get; private set; }
-        public TYPE BaseType { get => _baseType;}
+        public int CurrentHealth
+        {
+            get => _baseHealth;
+            private set { _baseHealth = value;  }
+        }
+        public TYPE BaseType
+        {
+            get => _baseType;
+            private set { BaseType = _baseType; }
+        }
         /// <summary>
         /// HPMax, prendre en compte base et equipement potentiel
         /// </summary>
         public int MaxHealth
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
+            get => _maxHealth;
+            private set { _maxHealth = value; }
         }
         /// <summary>
         /// ATK, prendre en compte base et equipement potentiel
         /// </summary>
         public int Attack
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
+            get => _baseAttack;
+            private set { _baseAttack = value; }
         }
         /// <summary>
         /// DEF, prendre en compte base et equipement potentiel
         /// </summary>
         public int Defense
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
+            get => _baseDefense;
+            private set { _baseDefense = value; }
         }
         /// <summary>
         /// SPE, prendre en compte base et equipement potentiel
         /// </summary>
         public int Speed
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
+            get => _baseSpeed;
+            private set { _baseSpeed = value; }
         }
         /// <summary>
         /// Equipement unique du personnage
@@ -90,7 +94,7 @@ namespace _2023_GC_A2_Partiel_POO.Level_2
         /// </summary>
         public StatusEffect CurrentStatus { get; private set; }
 
-        public bool IsAlive => throw new NotImplementedException();
+        public bool IsAlive => CurrentHealth > 0;
 
 
         /// <summary>
@@ -102,7 +106,11 @@ namespace _2023_GC_A2_Partiel_POO.Level_2
         /// <exception cref="NotImplementedException"></exception>
         public void ReceiveAttack(Skill s)
         {
-            throw new NotImplementedException();
+            _baseHealth -= s.Power - Defense;
+            if (_baseHealth < 0)
+            {
+                _baseHealth = 0;
+            }
         }
         /// <summary>
         /// Equipe un objet au personnage
@@ -111,14 +119,39 @@ namespace _2023_GC_A2_Partiel_POO.Level_2
         /// <exception cref="ArgumentNullException">Si equipement est null</exception>
         public void Equip(Equipment newEquipment)
         {
-            throw new NotImplementedException();
+            if (newEquipment == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            CurrentEquipment = newEquipment;
+            MaxHealth += newEquipment.BonusHealth;
+            Attack += newEquipment.BonusAttack;
+            Defense += newEquipment.BonusDefense;
+            Speed += newEquipment.BonusSpeed;
         }
         /// <summary>
         /// Desequipe l'objet en cours au personnage
         /// </summary>
         public void Unequip()
         {
-            throw new NotImplementedException();
+            MaxHealth -= CurrentEquipment.BonusHealth;
+            Attack -= CurrentEquipment.BonusAttack;
+            Defense -= CurrentEquipment.BonusDefense;
+            Speed -= CurrentEquipment.BonusSpeed;
+            CurrentEquipment = null;
+        }
+
+        public void Heal(int value)
+        {
+            if ((_baseHealth + value) > MaxHealth)
+            {
+                CurrentHealth = MaxHealth;
+            }
+            else
+            {
+                CurrentHealth += value;
+            }
         }
 
     }
--- Assets/_FightSystem/Level 2/Fight.cs
+++ Assets/_FightSystem/Level 2/Fight.cs
@@ -1,5 +1,5 @@
-﻿
-using System;
+﻿using System;
+using UnityEngine.TextCore.Text;
 
 namespace _2023_GC_A2_Partiel_POO.Level_2
 {
@@ -9,14 +9,23 @@ namespace _2023_GC_A2_Partiel_POO.Level_2
         {
             Character1 = character1;
             Character2 = character2;
+
+            if (Character1 == null)
+            {
+                throw new ArgumentNullException();
+            }
+            else if (Character2 == null)
+            {
+                throw new ArgumentNullException();
+            }
         }
 
-        public Character Character1 { get; }
-        public Character Character2 { get; }
+        public Character Character1 { get; private set; }
+        public Character Character2 { get; private set; }
         /// <summary>
         /// Est-ce la condition de victoire/défaite a été rencontré ?
         /// </summary>
-        public bool IsFightFinished => throw new NotImplementedException();
+        public bool IsFightFinished => Character1.CurrentHealth <= 0 || Character2.CurrentHealth <= 0;
 
         /// <summary>
         /// Jouer l'enchainement des attaques. Attention à bien gérer l'ordre des attaques par apport à la speed des personnages
@@ -26,8 +35,28 @@ namespace _2023_GC_A2_Partiel_POO.Level_2
         /// <exception cref="ArgumentNullException">si une des deux attaques est null</exception>
         public void ExecuteTurn(Skill skillFromCharacter1, Skill skillFromCharacter2)
         {
-            throw new NotImplementedException();
-        }
+            if(Character1.Speed >= Character2.Speed)
+            {
+                skillFromCharacter1.Power = Character1.Attack;
+                Character2.ReceiveAttack(skillFromCharacter1);
 
+                if (Character2.IsAlive)
+                {
+                    skillFromCharacter2.Power = Character2.Attack;
+                    Character1.ReceiveAttack(skillFromCharacter2);
+                }
+            }
+            else
+            {
+                skillFromCharacter2.Power = Character2.Attack;
+                Character1.ReceiveAttack(skillFromCharacter2);
+
+                if (Character1.IsAlive)
+                {
+                    skillFromCharacter1.Power = Character1.Attack;
+                    Character2.ReceiveAttack(skillFromCharacter1);
+                }
+            }
+        }
     }
 }
